cf108b97de8d2f2025066f3156f89ce182d294fb
manhdd <manhdd1991@gmail.com>
2023-06-28 16:56:22 +0700

    update pipeline

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,24 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=manhsech/mlpredict
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+r=$(kubectl get pod mlpredict-api 2> /dev/null;echo $?)
+
+if [ "_$r" == "_1" ] ; then
+    kubectl run mlpredict-api \
+    --image=$dockerpath \
+    --image-pull-policy="Always" \
+    --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
+fi
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlpredict-api 8080:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=manhsech/mlpredict
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u manhsech
+docker tag mlpredict-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
